302e4aa39869744ffc2496a81946a703dd2754a1
Helge Weissig <helgew@grajagan.org>
2020-02-13 09:15:14 -0800

    cleaned up licensing

--- src/main/java/org/grajagan/aws/AuthenticationHelper.java
+++ src/main/java/org/grajagan/aws/AuthenticationHelper.java
@@ -1,27 +1,5 @@
 package org.grajagan.aws;
 
-/*-
- * #%L
- * Emporia Energy API Client
- * %%
- * Copyright (C) 2002 - 2020 Helge Weissig
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
- */
-
 /*
 // Copyright 2013-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
