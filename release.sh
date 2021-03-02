b5dc8d513c8d1d44f1a7e8981bb93ae72da35c59
Helge Weissig <helgew@grajagan.org>
2021-03-02 15:02:02 -0800

    scm: assuming README.rst does not change

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 mvn clean release:clean release:prepare
 git commit -m'scm: updated README' README.rst
+git update-index --assume-unchanged README.rst
 git push origin master
 git checkout HEAD~2
 mvn clean generate-sources
