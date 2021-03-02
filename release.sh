491bc45802c68068057fae363e40c4fd053f3ff1
Helge Weissig <helgew@grajagan.org>
2021-03-02 13:34:49 -0800

    scm: fix to release script

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 mvn clean release:clean release:prepare
 git commit -m'scm: updated README' README.rst
 git push origin master
-git checkout HEAD~1
+git checkout HEAD~2
 mvn clean generate-sources
 mvn github-release:release
 git checkout master
